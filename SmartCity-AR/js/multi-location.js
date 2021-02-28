b91c4cb2e32a15c4d8d630db69a6bedcfc0b9653
Attila Toth <etotatt@HU-00003062.ericsson.se>
2021-02-28 22:16:26 +0100

    Refreshing

--- SmartCity-AR/js/multi-location.js
+++ SmartCity-AR/js/multi-location.js
@@ -13,8 +13,8 @@ function staticLoadPlaces() {
         {
             name: 'Pokèmon',
             location: {
-                // lat: <your-latitude>,
-                // lng: <your-longitude>,
+                lat: 47.5111631,// lat: <your-latitude>,
+                lng: 19.0360759 // lng: <your-longitude>,
             },
         },
     ];
